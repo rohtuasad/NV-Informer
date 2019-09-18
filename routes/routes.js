485b12f6de0fdb288161a7f7f390dd63d0ae244c
Alexey Volkov <rohtuasad@gmail.com>
2019-09-18 23:59:04 +0300

    Add signup page

--- routes/routes.js
+++ routes/routes.js
@@ -2,4 +2,7 @@ module.exports = function(app) {
   app.get("/", function(req, res) {
     res.render("index");
   });
+  app.get("/signup", function(req, res) {
+    res.render("signup");
+  });
 };
