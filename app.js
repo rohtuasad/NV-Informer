b8b390bd1c6371910ffc74b15a78c091d94ead19
Alexey Volkov <rohtuasad@gmail.com>
2019-09-18 23:39:37 +0300

    Change routes method

--- app.js
+++ app.js
@@ -4,9 +4,6 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-
 var configDB = require("./config/database.js");
 var mongoose = require("mongoose");
 
@@ -17,15 +14,13 @@ var app = express();
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+require("./routes/routes.js")(app);
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
