84e04561accbae5a469206c18b3b65d38333aeb3
Alexey Volkov <rohtuasad@gmail.com>
2019-09-22 18:06:44 +0300

    Add permission check stub

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var mongoose = require("mongoose");
 var passport = require("passport");
 var flash = require("connect-flash");
 var session = require("express-session");
+var permissions = require("./src/authorization/permissions.js");
 
 mongoose.connect(configDB.url);
 
@@ -29,6 +30,7 @@ app.use(session({ secret: "ilovescotchscotchyscotchscotch" }));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
+app.use(permissions);
 require("./src/routes/routes.js")(app, passport);
 
 // catch 404 and forward to error handler
