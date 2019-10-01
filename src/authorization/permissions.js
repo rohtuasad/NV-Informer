041586228c701c093c477b5276536021be52fdab
Alexey Volkov <rohtuasad@gmail.com>
2019-10-01 22:06:00 +0300

    Add role check on gangsters page

--- src/authorization/permissions.js
+++ src/authorization/permissions.js
@@ -3,9 +3,23 @@ var roles = require("./roles");
 module.exports = function(req, res, next) {
   switch (req.url) {
     case "/grant": {
-      if (req.user && !req.user.local.roles.includes(roles.Grant)) {
+      if (
+        !req.user ||
+        (req.user && !req.user.local.roles.includes(roles.Grant))
+      ) {
         res.redirect("/");
       }
+      break;
+    }
+    case "/gangsters": {
+      console.log(req.user);
+      if (
+        !req.user ||
+        (req.user && !req.user.local.roles.includes(roles.User))
+      ) {
+        res.redirect("/");
+      }
+      break;
     }
   }
   next();
