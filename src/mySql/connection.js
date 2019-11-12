b9e9e62b55feaa4aa97c668ce33d2a415855dbca
Alexey Volkov <rohtuasad@gmail.com>
2019-11-12 22:38:01 +0300

    refact - query executions

--- src/mySql/connection.js
+++ src/mySql/connection.js
@@ -18,27 +18,15 @@ exports.getGangsters = function(callback) {
 
     console.log("connected as id " + connection.threadId);
 
-    connection.query("SET NAMES 'utf8mb4'", (err, response) => {
-      if (err) {
-        console.error(err);
-      }
-    });
+    setUtf8Names(connection);
 
-    connection.query(
-      `select g.name, s.health, s.strength, s.agility, s.charisma, s.accuracy, s.damage, s.dzen, g.tlgname from states s, gangster g, (SELECT
-        gangsterid, MAX(time) as time
-      FROM
-        states 
-      GROUP BY
-        gangsterid) as ls where g.gangid in (3, 4, 5, 6) and s.time = ls.time and s.gangsterid = ls.gangsterid and s.gangsterid = g.id order by dzen desc`,
-      function(err, rows) {
-        connection.release();
-        if (err) {
-          rows = {};
-        }
-        callback(rows);
-      }
-    );
+    var query = `select g.name, s.health, s.strength, s.agility, s.charisma, s.accuracy, s.damage, s.dzen, g.tlgname from states s, gangster g, (SELECT
+      gangsterid, MAX(time) as time
+    FROM
+      states 
+    GROUP BY
+      gangsterid) as ls where g.gangid in (3, 4, 5, 6) and s.time = ls.time and s.gangsterid = ls.gangsterid and s.gangsterid = g.id order by dzen desc`;
+    doQuery(connection, query, callback);
 
     connection.on("error", function(err) {
       callback({});
@@ -56,27 +44,15 @@ exports.getGangstersShort = function(callback) {
 
     console.log("connected as id " + connection.threadId);
 
-    connection.query("SET NAMES 'utf8mb4'", (err, response) => {
-      if (err) {
-        console.error(err);
-      }
-    });
+    setUtf8Names(connection);
 
-    connection.query(
-      `select g.id, g.name, g.group_id, (s.health + s.agility + s.charisma + s.accuracy + s.damage) as bm, s.dzen from states s, gangster g, (SELECT
+    var query = `select g.id, g.name, g.group_id, (s.health + s.agility + s.charisma + s.accuracy + s.damage) as bm, s.dzen from states s, gangster g, (SELECT
         gangsterid, MAX(time) as time
       FROM
         states 
       GROUP BY
-        gangsterid) as ls where g.gangid in (3, 4, 5, 6) and s.time = ls.time and s.gangsterid = ls.gangsterid and s.gangsterid = g.id order by dzen desc`,
-      function(err, rows) {
-        connection.release();
-        if (err) {
-          rows = {};
-        }
-        callback(rows);
-      }
-    );
+        gangsterid) as ls where g.gangid in (3, 4, 5, 6) and s.time = ls.time and s.gangsterid = ls.gangsterid and s.gangsterid = g.id order by dzen desc`;
+    doQuery(connection, query, callback);
 
     connection.on("error", function(err) {
       callback({});
@@ -94,25 +70,14 @@ exports.getRaidTop = function(callback) {
 
     console.log("connected as id " + connection.threadId);
 
-    connection.query("SET NAMES 'utf8mb4'", (err, response) => {
-      if (err) {
-        console.error(err);
-      }
-    });
+    setUtf8Names(connection);
 
-    connection.query(
-      `select name, SUM(if(placed = true, 1, 0)) as placed, SUM(if(participation = true, 1, 0)) as raids from (select g.name, f.placed, f.participation from raidFact f, raid r, gangster g where 
-        f.raidId = r.id and f.gangsterId = g.id and r.id > ((select max(raidid) from raidFact) - 42)) as w group by name`,
-      function(err, rows) {
-        connection.release();
-        if (err) {
-          rows = {};
-        }
-        callback(rows);
-      }
-    );
+    var query = `select name, SUM(if(placed = true, 1, 0)) as placed, SUM(if(participation = true, 1, 0)) as raids from (select g.name, f.placed, f.participation from raidFact f, raid r, gangster g where 
+        f.raidId = r.id and f.gangsterId = g.id and g.gangid in (3, 4, 5, 6) and r.id > ((select max(raidid) from raidFact) - 42)) as w group by name`;
+    doQuery(connection, query, callback);
 
     connection.on("error", function(err) {
+      console.log(err);
       callback({});
       return;
     });
@@ -128,11 +93,7 @@ exports.updateGroups = function(groups, callback) {
 
     console.log("connected as id " + connection.threadId);
 
-    connection.query("SET NAMES 'utf8mb4'", (err, response) => {
-      if (err) {
-        console.error(err);
-      }
-    });
+    setUtf8Names(connection);
 
     var updateQuery = "";
 
@@ -147,14 +108,7 @@ exports.updateGroups = function(groups, callback) {
       }
     }
 
-    connection.query(updateQuery, function(err, rows) {
-      connection.release();
-      if (err) {
-        console.error(err);
-        rows = {};
-      }
-      callback({ success: true });
-    });
+    doQuery(connection, updateQuery, callback);
 
     connection.on("error", function(err) {
       callback({});
@@ -172,25 +126,14 @@ exports.getGroupIds = function(num, callback) {
 
     console.log("connected as id " + connection.threadId);
 
-    connection.query("SET NAMES 'utf8mb4'", (err, response) => {
-      if (err) {
-        console.error(err);
-      }
-    });
+    setUtf8Names(connection);
 
     var query = mysql.format(
       `select g.name, g.tlgid from gangster g where g.group_id = ? and g.gangid in (3, 4, 5, 6)`,
       num
     );
-    console.log(query);
-    connection.query(query, function(err, rows) {
-      connection.release();
-      if (err) {
-        console.error(err);
-        rows = {};
-      }
-      callback(rows);
-    });
+    
+    doQuery(connection, query, callback);
 
     connection.on("error", function(err) {
       callback({});
@@ -198,3 +141,22 @@ exports.getGroupIds = function(num, callback) {
     });
   });
 };
+
+function doQuery(connection, query, callback) {
+  connection.query(query, function(err, rows) {
+    connection.release();
+    if (err) {
+      console.error(err);
+      rows = {};
+    }
+    callback(rows);
+  });
+}
+
+function setUtf8Names(connection) {
+  connection.query("SET NAMES 'utf8mb4'", (err, response) => {
+    if (err) {
+      console.error(err);
+    }
+  });
+}
